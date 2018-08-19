13a5912400fc94ba63c3809391828bfa95dca581
hrzyusoff <hrzyusoff@gmail.com>
2018-08-19 14:50:53 +0800

    first commit for main site

---
+++ plugin/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $('#pagepiling').pagepiling({
+        menu: null,
+        direction: 'vertical',
+        verticalCentered: true,
+        sectionsColor: [],
+        anchors: ['home', 'about', 'skills', 'education', 'experience', 'portfolio', 'contact'],
+        scrollingSpeed: 700,
+        easing: 'swing',
+        loopBottom: false,
+        loopTop: false,
+        css3: true,
+        navigation: {
+            'textColor': '#333333',
+            'bulletsColor': '#333333',
+            'position': 'right',
+            'tooltips': ['Home', 'About', 'Skills', 'Education', 'Experience', 'Portfolio', 'Contact']
+        },
+        normalScrollElements: null,
+        normalScrollElementTouchThreshold: 5,
+        touchSensitivity: 5,
+        keyboardScrolling: true,
+        sectionSelector: '.section',
+        animateAnchor: true,
+
+        //events
+        onLeave: function (index, nextIndex, direction) { },
+        afterLoad: function (anchorLink, index) { },
+        afterRender: function () { },
+    });
+});
+
